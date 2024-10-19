7cad2b973e8fcdcdb0cc5001a368d7dab1387663
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 09:00:15 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerificarDesconto from './components/VerificarDesconto';
+
+function App() {
+  return (
+    <div className="App">
+      <VerificarDesconto temDesconto={true} />
+    </div>
+  );
+}
+
+export default App;
